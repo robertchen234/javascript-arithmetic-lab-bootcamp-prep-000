ba062fe872591062e2e104f1629d18b550fa6211
Robert Chen <robertchen234@gmail.com>
2018-08-18 00:34:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function add() {
-  var a = 800
-  var b = 78
   a + b
-  return(add)
 }
 
 function subtract() {
-  return(subtract)
+  
+}
+
+function multiplay() {
+  
+}
+
+function divide() {
+  
 }
